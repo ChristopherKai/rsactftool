e4fe89e7ca8741597d5b21f4713c25eda549f557
kais <kaisong39@gmail.com>
2021-08-01 11:43:40 +0800

    2

---
+++ rsacreate.bat
@@ -0,0 +1,1 @@
+docker run --rm -it -u root -v /c/Users/mount/kali/root/crypto:/home/sage/RsaCtfTool/ctfs registry.cn-hangzhou.aliyuncs.com/kais_2020/rsactftool bash
---
+++ rsacreate.sh
@@ -0,0 +1,1 @@
+docker run --rm -it -u root -v $(pwd):/home/sage/RsaCtfTool/ctfs deltaclock/rsactftool bash
